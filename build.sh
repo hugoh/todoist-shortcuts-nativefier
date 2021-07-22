3d0b06469926f97aaed15008a3d5212ae5f4e6f8
Hugo Haas <hugoh@hugoh.net>
2021-07-22 18:33:04 -0500

    No bounce and fast quit

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 git submodule update --init --recursive
 COUNTER_JS=./counter.js
 TODOIST_SHORTCUTS_JS=./todoist-shortcuts/src/todoist-shortcuts.js
-./node_modules/.bin/nativefier 'https://todoist.com/app' --name 'Todoist' --icon ./todoist-icon.png --inject $TODOIST_SHORTCUTS_JS --inject $COUNTER_JS --counter --bounce --single-instance
+./node_modules/.bin/nativefier 'https://todoist.com/app' --name 'Todoist' --icon ./todoist-icon.png --inject $TODOIST_SHORTCUTS_JS --inject $COUNTER_JS --counter --single-instance --fast-quit
